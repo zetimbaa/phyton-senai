308002f8e769deef2a890c61d3df366f5f7ed29e
zetimbaa <kaiquiprofissi@gmail.com>
2024-12-05 11:26:02 -0300

    aula 2-variaveis

---
+++ aula 02/aula2.py
@@ -0,0 +1,8 @@
+nome_usuario='zeca pagodinho'#por que ele prefere garoto
+idade_usuario=64
+altura_usuario=1.74
+maior_idade=True
+print('seja bem vindo',nome_usuario)
+print ('sabe pq padre n gosta de nestle?')
+print('pq ele prefere garoto')
+print('sua idade é',idade_usuario,'anos, você tem',altura_usuario,'metros. Você é maior de idade?',maior_idade)
